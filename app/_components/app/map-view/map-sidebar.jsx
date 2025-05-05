4410779d84ce6a87eae921bc6fb694b9ebf2ba39
Kahfi Zulkifli <sbhnkahfi@gmail.com>
2025-05-05 21:09:24 +0700

    refactor and added button sidebar

--- app/_components/app/map-view/map-sidebar.jsx
+++ app/_components/app/map-view/map-sidebar.jsx
@@ -9,83 +9,88 @@ import LayersContent from "./map-sidebar/layers-content";
 import { ButtonSidebar } from "@/components/ui/button-sidebar";
 
 const MapSidebar = () => {
+
   const [showSidebar, setShowSidebar] = useState(true);
-  return (
-    <div
-      className={cn(
-        "flex flex-col fixed top-[56px] h-[calc(100vh-56px)] left-0 bottom-10 z-10 bg-gn-400 w-[48px]",
-        {
-          "w-[180px]": !showSidebar,
-        },
-      )}
-    >
-        <ButtonSidebar variant="ghost">
-          <PlusCircle
-            className={cn("w-4 h-4")}
-          />
-          {!showSidebar && <span className="ml-2 inline-block">Add layers</span>}
-        </ButtonSidebar>
-        <ButtonSidebar variant="ghost">
-          <Layers3
-            className={cn("w-4 h-4")}
-          />
-          {!showSidebar && <span className="ml-2 inline-block">Layers</span>}
-        </ButtonSidebar>
-        <ButtonSidebar variant="ghost">
-          <img
-            className={cn("w-4 h-4")}
-            src="/app/basemap-svgrepo-com.svg"
-            alt="basemap icon"
-          />
-          {!showSidebar && <span className="ml-2 inline-block">Basemap</span>}
-        </ButtonSidebar>
-        <ButtonSidebar variant="ghost">
-          <Sheet
-            className={cn("w-4 h-4")}
-          />
-          {!showSidebar && <span className="ml-2 inline-block">Tables</span>}
-        </ButtonSidebar>
-        <ButtonSidebar variant="ghost">
-          <Save
-            className={cn("w-4 h-4")}
-          />
-          {!showSidebar && <span className="ml-2 inline-block">Save</span>}
-        </ButtonSidebar>
-        <ButtonSidebar variant="ghost">
-          <Share2
-            className={cn("w-4 h-4")}
-          />
-          {!showSidebar && <span className="ml-2 inline-block">Share</span>}
-        </ButtonSidebar>
-        <ButtonSidebar variant="ghost">
-          <Printer
-            className={cn("w-4 h-4")}
-          />
-          {!showSidebar && <span className="ml-2 inline-block">Print</span>}
-        </ButtonSidebar>
-        <ButtonSidebar variant="ghost" onClick={() => setShowSidebar((prev) => !prev)}>
-          <ChevronLeft
-            className={cn("w-4 h-4", {
-              "-rotate-180": showSidebar,
-            })}
-          />
-          {!showSidebar && <span className="ml-2 inline-block">Collapse</span>}
-        </ButtonSidebar>
-      {/* </div> */}
-      {/* <Tabs defaultValue="layers" className="w-full mt-2">
-        <TabsList className="flex justify-between w-full">
-          <TabsTrigger value="layers" className="w-1/2">
-            Layers
-          </TabsTrigger>
-          <TabsTrigger value="legend" className="w-1/2">
-            Legend
-          </TabsTrigger>
-        </TabsList>
-        <LayersContent />
-        <LegendContent />
-      </Tabs> */}
-    </div>
+  const [showButtonSidebar, setShowButtonSidebar] = useState(true);
+  const [selectedButton, setSelectedButton] = useState(null);
+
+  // Reusable Button component
+  const Button = ({ icon, label, onClick }) => (
+    <ButtonSidebar variant="ghost" onClick={onClick}>
+      {icon}
+      {!showSidebar && <span className="ml-2 inline-block">{label}</span>}
+    </ButtonSidebar>
   );
-};
+
+
+  // Define content components for each button
+  const AddContent = () => <div>Add content goes here</div>;
+  const LayersContent = () => <div>Layers content goes here</div>;
+  const BasemapContent = () => <div>Basemap content goes here</div>;
+  const TablesContent = () => <div>Tables content goes here</div>;
+  const SaveContent = () => <div>Save content goes here</div>;
+  const ShareContent = () => <div>Share content goes here</div>;
+  const PrintContent = () => <div>Print content goes here</div>;
+  const CollapseContent = () => <div>Collapse content goes here</div>;
+
+  // Define content for each button
+  const buttonContent = {
+    add: <AddContent />,
+    layers: <LayersContent />,
+    basemap: <BasemapContent />,
+    tables: <TablesContent />,
+    save: <SaveContent />,
+    share: <ShareContent />,
+    print: <PrintContent />,
+    collapse: <CollapseContent />,
+  };
+
+  const handleButtonClick = (buttonName) => {
+    setSelectedButton(buttonName === selectedButton ? null : buttonName);
+  };
+
+  // Button data
+  const buttons = [
+    { icon: <PlusCircle className={cn("w-4 h-4")} />, label: 'Add', onClick: () =>  handleButtonClick('add')},
+    { icon: <Layers3 className={cn("w-4 h-4")} />, label: 'Layers', onClick: () =>  handleButtonClick('layers')},
+    { icon: <img className={cn("w-4 h-4")} src="/app/basemap-svgrepo-com.svg" alt="basemap icon" />, label: 'Basemap', onClick: () =>  handleButtonClick('basemap') },
+    { icon: <Sheet className={cn("w-4 h-4")} />, label: 'Tables', onClick:  () =>  handleButtonClick('tables') },
+    { icon: <Save className={cn("w-4 h-4")} />, label: 'Save', onClick:  () =>  handleButtonClick('save') },
+    { icon: <Share2 className={cn("w-4 h-4")} />, label: 'Share', onClick:  () =>  handleButtonClick('share') },
+    { icon: <Printer className={cn("w-4 h-4")} />, label: 'Print', onClick:  () =>  handleButtonClick('print') },
+    { icon: <ChevronLeft className={cn("w-4 h-4", { "-rotate-180": showSidebar })} />, label: 'Collapse', onClick: () => setShowSidebar(prev => !prev) },
+  ];
+
+    return (
+      <div className="relative">
+        <div
+          className={cn(
+            "flex flex-col fixed top-[56px] h-[calc(100vh-56px)] left-0 bottom-10 z-10 bg-gn-400 w-[48px]",
+            {
+              "w-[160px]": !showSidebar,
+            },
+          )}
+        >
+            {/* Render buttons dynamically */}
+          {buttons.map((button, index) => (
+            <Button key={index} {...button} />
+          ))}
+        </div>
+        {selectedButton && (
+          <div
+            className={cn(
+              "flex flex-col fixed top-[56px] h-[calc(100vh-56px)] bottom-10 z-10 bg-white",
+              {
+                "left-12 w-60": showSidebar,
+                "left-40 w-60": !showSidebar,
+              },
+            )}
+          >
+            {buttonContent[selectedButton]}
+          </div>
+        )}
+      </div>
+    );
+  };
 
 export default MapSidebar;
