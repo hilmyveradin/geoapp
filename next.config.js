f62936840df5c7ec726866609bd49024f4d48eba
hilmyveradin <hilmyveradin@gmail.com>
2025-05-05 21:09:26 +0700

    delete debugger and fix layer content ordering

--- next.config.js
+++ next.config.js
@@ -1,16 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // Define hosts that have public images
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'http',
-                hostname: 'dev3.webgis.co.id',
-                port: '',
-                pathname: '/be/gs/thumbnail/***',
-            },
-        ],
-    },
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "dev3.webgis.co.id",
+        port: "",
+        pathname: "/be/gs/thumbnail/***",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
