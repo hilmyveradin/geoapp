0a00bb1043e50271220005516616a6af70cda83d
hilmyveradin <hilmyveradin@gmail.com>
2025-05-05 21:09:32 +0700

    feat: modify delete layer functionality

--- app/_components/app/menu-card-dropdown.jsx
+++ app/_components/app/menu-card-dropdown.jsx
@@ -8,9 +8,15 @@ import {
 import { PencilIcon } from "lucide-react";
 import ChangePropDialog from "./shared/change-prop-dialog";
 import { useState } from "react";
-
+import DestructiveDialog from "../shared/DestructiveDialog";
+import useRefetchStore from "@/helpers/hooks/store/useRefetchStore";
+import { Trash2 } from "lucide-react";
+import { handleErrorMessage } from "@/helpers/string-helpers";
+import { useToast } from "@/components/ui/use-toast";
 const MenuCardDropdown = ({ children, cardData }) => {
   const [openDropdown, setOpenDropdown] = useState(false);
+  const { toggleRefetchLayers, toggleRefetchMaps } = useRefetchStore();
+  const { toast } = useToast();
 
   const handleDropdownClick = (e) => {
     // Stop the click event from propagating up to parent elements
@@ -19,6 +25,65 @@ const MenuCardDropdown = ({ children, cardData }) => {
 
   const handleCloseDropdown = () => {
     setOpenDropdown(false);
+  };
+
+  const deleteAction = async () => {
+    if (cardData.cardType === "map") {
+      try {
+        const response = await fetch(
+          `/api/maps/delete-map?mapUid=${cardData.cardUid}`,
+          {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+          }
+        );
+
+        if (!response.ok) {
+          const error = new Error(response.statusText || "Unknown error");
+          error.status = response.status;
+          throw error;
+        }
+
+        toast({ title: "Success deleting maps", variant: "success" });
+        toggleRefetchMaps();
+        handleCloseDropdown();
+      } catch (error) {
+        const { title, description } = handleErrorMessage(error.status);
+        toast({
+          title: title,
+          description: description, // Provides more specific error detail
+          variant: "destructive",
+        });
+      }
+    } else {
+      try {
+        // TODO: Fix this layer code. The payload shouldn't have any layer_uid from BE
+        const response = await fetch(
+          `/api/layers/delete-layer?layerUid=${cardData.cardUid}`,
+          {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+          }
+        );
+
+        if (!response.ok) {
+          const error = new Error(response.statusText || "Unknown error");
+          error.status = response.status;
+          throw error;
+        }
+
+        toast({ title: "Success deleting layer", variant: "success" });
+        toggleRefetchLayers();
+      } catch (error) {
+        console.error("Error during fetch:", error.message);
+        const { title, description } = handleErrorMessage(error.status);
+        toast({
+          title: title,
+          description: description, // Provides more specific error detail
+          variant: "destructive",
+        });
+      }
+    }
   };
 
   return (
@@ -51,7 +116,7 @@ const MenuCardDropdown = ({ children, cardData }) => {
             </button>
           </ChangePropDialog>
         </DropdownMenuItem>
-        <DropdownMenuItem asChild onClick={handleDropdownClick}>
+        {/* <DropdownMenuItem asChild onClick={handleDropdownClick}>
           <button className="flex w-full">
             <img
               src="/app/share-people-icon.svg"
@@ -60,16 +125,19 @@ const MenuCardDropdown = ({ children, cardData }) => {
             />
             <p>Share</p>
           </button>
-        </DropdownMenuItem>
+        </DropdownMenuItem> */}
         <DropdownMenuItem asChild onClick={handleDropdownClick}>
-          <button className="flex w-full">
-            <img
-              src="/app/delete-icon.svg"
-              alt="delete icon"
-              className="w-5 h-5 mr-3"
-            />
-            <p>Delete</p>
-          </button>
+          <DestructiveDialog
+            title={`Are you sure you want to delete this ${cardData.cardType}`}
+            description="This action cannot be undone"
+            actionText="Yes, I'm sure"
+            action={() => deleteAction()}
+          >
+            <button className="flex justify-left w-full p-1 text-md hover:bg-slate-100 relative cursor-default select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none transition-colors focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50">
+              <Trash2 className="w-4 h-4 mr-3" />
+              <p>{`Delete ${cardData.cardType}`}</p>
+            </button>
+          </DestructiveDialog>
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
--- app/api/layers/delete-layer/route.js
+++ app/api/layers/delete-layer/route.js
@@ -4,18 +4,19 @@ import authOptions from "../../auth/[...nextauth]/options";
 
 export async function POST(request) {
   try {
-    const body = await request.json();
-    const { layerUids } = body;
+    const layerUid = request.nextUrl.searchParams.get("layerUid");
     const session = await getServerSession(authOptions);
 
-    const res = await fetch(`${process.env.API_BASE_URL}/cms/layer/delete`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${session.accessToken}`,
-      },
-      body: JSON.stringify({ layers: layerUids }),
-    });
+    const res = await fetch(
+      `${process.env.API_BASE_URL}/cms/layer/${layerUid}/delete`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${session.accessToken}`,
+        },
+      }
+    );
 
     if (!res.ok) {
       console.error(
