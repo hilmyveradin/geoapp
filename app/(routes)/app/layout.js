9ae42ddee19c30147b57bdff84e0bd0b86db1d57
Kahfi Zulkifli <sbhnkahfi@gmail.com>
2025-05-05 21:09:22 +0700

    Add button and file drop

--- app/(routes)/app/layout.js
+++ app/(routes)/app/layout.js
@@ -4,7 +4,7 @@ const AppLayout = ({ children }) => {
   return (
     <div className="absolute top-0 bottom-0 left-0 right-0 flex mx-1 my-2">
       <AppSidebar />
-      <div className="w-full h-full bg-blue-50">{children}</div>
+      <div className="w-screen h-screen bg-[#F7FFFB]">{children}</div>
     </div>
   );
 };
