6c3cc6f4300bd790bdf86af8fc82efa690545786
hilmyveradin <hilmyveradin@gmail.com>
2025-05-05 21:09:34 +0700

    modify ci/cd and add IFC loader

---
+++ app/(routes)/ifc-cesium-demo/page.js
@@ -0,0 +1,9 @@
+"use client";
+
+import IFCCesiumMap from "@/app/_components/app/map-view/ifc-cesium-demo";
+
+const CesiumDemo = () => {
+  return <IFCCesiumMap />;
+};
+
+export default CesiumDemo;
