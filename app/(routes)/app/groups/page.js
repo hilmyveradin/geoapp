1f157c679292cecda54162e4c15d5296a6b09aa6
hilmyveradin <hilmyveradin@gmail.com>
2025-05-05 21:09:33 +0700

    base view for group cards

--- app/(routes)/app/groups/page.js
+++ app/(routes)/app/groups/page.js
@@ -1,13 +1,72 @@
 "use client";
 
 import GroupButtons from "@/app/_components/app/group-buttons";
+import GroupCards from "@/app/_components/app/group-cards";
+import useRefetchStore from "@/helpers/hooks/store/useRefetchStore";
 import { Loader2 } from "lucide-react";
-import { useState } from "react";
+import { useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
 
 const GruopsDashboard = () => {
   const [pageLoading, setPageLoading] = useState(true);
+  const [groupsData, setGroupsData] = useState([]);
 
-  if (!pageLoading) {
+  const { refetchGroups, toggleRefetchGroups } = useRefetchStore();
+
+  useEffect(() => {
+    async function getGroupInfo(groupUid) {
+      const response = await fetch(
+        `/api/groups/get-group-info?groupUid=${groupUid}`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      const resposneData = await response.json();
+
+      return resposneData.data[0];
+    }
+    // Define function to get layers API
+    async function getGroupLists() {
+      try {
+        const response = await fetch("/api/groups/get-groups", {
+          method: "GET",
+        });
+
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        const responseData = await response.json();
+        const data = responseData.data;
+
+        // Create an array of promises
+        const groupDataPromises = data.map((groupAttributes) =>
+          getGroupInfo(groupAttributes.groupUid)
+        );
+
+        // Wait for all promises to resolve
+        const resolvedGroupData = await Promise.all(groupDataPromises);
+
+        // Flatten the array of arrays (if necessary) and set the state
+        const groupData = resolvedGroupData.flat(); // Use .flat() if each promise resolves to an array
+
+        setGroupsData(groupData);
+      } catch (error) {
+        console.error("Error during fetch:", error.message);
+      } finally {
+        setPageLoading(false);
+      }
+    }
+
+    getGroupLists()
+      // make sure to catch any error
+      .catch(console.error);
+  }, [refetchGroups]);
+
+  if (pageLoading) {
     return (
       <div className="flex items-center justify-center w-full h-screen">
         <Loader2 className="w-10 h-10 stroke-blackHaze-500 animate-spin" />
@@ -18,6 +77,9 @@ const GruopsDashboard = () => {
   return (
     <div className="w-full h-full px-8 mt-4">
       <GroupButtons />
+      {groupsData.map((group, index) => {
+        return <GroupCards key={index} group={group} />;
+      })}
     </div>
   );
 };
--- app/_components/app/group-cards.jsx
+++ app/_components/app/group-cards.jsx
@@ -1,21 +1,41 @@
-const GroupCards = () => {
-  <div className="flex">
-    <img className="w-40 h-40" />
-    <div className="flex flex-col">
-      <p className="text-2xl font-semibold text-nileBlue-700"> Group Admin </p>
-      <div className="flex">
-        <p>owner</p>
-        <p>user</p>
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useSession } from "next-auth/react";
+
+const GroupCards = (props) => {
+  const { data: session, status } = useSession();
+  const { group } = props;
+
+  return (
+    <div className="flex items-center gap-4 p-2 mt-2 rounded-md shadow-md">
+      <img className="!w-40 !h-40" />
+      <div className="flex flex-col w-full">
+        <p className="text-2xl font-semibold text-nileBlue-700">
+          {group.groupName}
+        </p>
+        <div className="flex gap-4">
+          <p>owner</p>
+          <p className="text-nileBlue-700">user</p>
+        </div>
+        <div className="flex gap-4">
+          <p>created</p>
+          <p>{group.startDate ?? "-"}</p>
+          <p>last update</p>
+          <p>{group.endData ?? "-"}</p>
+        </div>
       </div>
-      <div className="flex">
-        <p>created</p>
-        <p>date</p>
-        <p>last update</p>
-        <p>abcdef</p>
-      </div>
+      {group.users.includes(session.user.userUid) ? (
+        <Button variant="secondary">
+          <span>leave group</span>
+        </Button>
+      ) : (
+        <Button>
+          <span>delete group</span>
+        </Button>
+      )}
     </div>
-    <button>leave group</button>
-  </div>;
+  );
 };
 
 export default GroupCards;
