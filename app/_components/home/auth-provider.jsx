95d2dadac1b3dc4375739cc14aa0ccadc3bd30c8
hilmyveradin <hilmyveradin@gmail.com>
2025-05-05 21:09:34 +0700

    add basepath

--- app/_components/home/auth-provider.jsx
+++ app/_components/home/auth-provider.jsx
@@ -3,7 +3,11 @@
 import { SessionProvider } from "next-auth/react";
 
 const AuthProvider = ({ children }) => {
-  return <SessionProvider>{children}</SessionProvider>;
+  return (
+    <SessionProvider basePath={process.env.NEXT_PUBLIC_BASE_PATH}>
+      {children}
+    </SessionProvider>
+  );
 };
 
 export default AuthProvider;
