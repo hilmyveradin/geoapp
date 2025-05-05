5fe5113a4462d43b712384d3e5a4f0d6a0ebe776
hilmyveradin <hilmyveradin@gmail.com>
2025-05-05 21:09:24 +0700

    fix: map button color

--- app/_components/app/top-bar-action.jsx
+++ app/_components/app/top-bar-action.jsx
@@ -1,15 +1,27 @@
 import { Button } from "@/components/ui/button";
 import ComposeMapDialog from "./compose-map-dialog";
+import { Filter } from "lucide-react";
+import { Plus } from "lucide-react";
+import { ArrowDownWideNarrow } from "lucide-react";
 
 const TopBarAction = () => {
   return (
-    <div className="flex items-center justify-between w-full">
-      <Button>Filter</Button>
+    <div className="flex items-center justify-between w-full px-4 mt-2">
+      <Button variant="secondary" className="flex gap-2">
+        <Filter className="w-5 h-5" fill="#006236" />
+        Filter
+      </Button>
       <div className="flex items-center justify-center gap-4">
         <ComposeMapDialog>
-          <Button>Add Items</Button>
+          <Button className="flex gap-2">
+            <Plus className="w-5 h-5 stroke-[4px]" fill="white" />
+            Add Map
+          </Button>
         </ComposeMapDialog>
-        <Button>Sort By</Button>
+        <Button variant="secondary" className="flex gap-2">
+          <ArrowDownWideNarrow className="w-5 h-5" fill="#006236" />
+          Sort By
+        </Button>
       </div>
     </div>
   );
