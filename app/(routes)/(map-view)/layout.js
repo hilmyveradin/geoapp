d1a87473c6c94094f4f550f87fd3455886cfffc5
hilmyveradin <hilmyveradin@gmail.com>
2025-05-05 21:09:35 +0700

    add redirect to login page if not authenticated

--- app/(routes)/(map-view)/layout.js
+++ app/(routes)/(map-view)/layout.js
@@ -2,19 +2,19 @@
 
 import AppHeader from "@/app/_components/app/shared/header";
 import { Toaster } from "@/components/ui/toaster";
-// import { useSession } from "next-auth/react";
-// import { useRouter } from "next/navigation";
-// import { useEffect } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 const MapViewLayout = ({ children }) => {
   // TODO: Find how to handle this based on server
-  // const { data: session, status } = useSession();
-  // const router = useRouter();
+  const { data: session, status } = useSession();
+  const router = useRouter();
 
-  // useEffect(() => {
-  //   if (status === "loading") return; // Do nothing while loading
-  //   if (!session) router.replace("/"); // Redirect to login if not authenticated
-  // }, [session, status, router]);
+  useEffect(() => {
+    if (status === "loading") return; // Do nothing while loading
+    if (!session) router.replace("/"); // Redirect to login if not authenticated
+  }, [session, status, router]);
 
   return (
     // <div className="absolute top-0 bottom-0 left-0 right-0 flex flex-col mx-1 my-2">
